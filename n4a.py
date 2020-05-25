df93ea4a6b28eacc6448b71481d0f82c86078cb5
Adrian Sampson <adrian@radbox.org>
2020-05-25 12:17:28 -0400

    Cache results

--- n4a.py
+++ n4a.py
@@ -2,11 +2,13 @@ import requests
 import csv
 from contextlib import closing
 from datetime import datetime
+import json
 
 CSV_URL = 'https://projects.fivethirtyeight.com/trump-approval-data/' \
     'approval_topline.csv'
 LINK_URL = 'https://projects.fivethirtyeight.com/trump-approval-ratings/'
 SUBGROUP = 'All polls'
+LAST_UPDATE_FILE = 'last_update.json'
 
 
 def get_model():
@@ -17,11 +19,39 @@ def get_model():
                 yield row
 
 
+def checkpoint(filename, data):
+    """Check whether the dict `data` differs from the last time this
+    function was called, saving the result as JSON in `filename`.
+    """
+    try:
+        with open(filename) as f:
+            old_data = json.load(f)
+    except (IOError, json.JSONDecodeError):
+        changed = True
+    else:
+        changed = not all(data[k] == old_data[k] for k in data)
+
+    with open(filename, 'w') as f:
+        json.dump(data, f)
+
+    return changed
+
+
 def n4a():
+    # Get the latest model data.
     latest = next(get_model())
     modeldate = datetime.strptime(latest['modeldate'], '%m/%d/%Y')
     approve = float(latest['approve_estimate'])
     disapprove = float(latest['disapprove_estimate'])
+
+    # Check whether anything has changed.
+    changed = checkpoint(LAST_UPDATE_FILE, {
+        'modeldate': modeldate.timestamp(),
+        'approve': '{:.1f}'.format(approve),
+        'disapprove': '{:.1f}'.format(disapprove),
+    })
+    if not changed:
+        return
 
     msg = 'As of {}:\n{:.1f}% approve\n{:.1f}% disapprove\n{}'.format(
         modeldate.strftime('%A, %B %-d, %Y'),
